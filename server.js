e37b61693f83d002e6e6fdfa215a6cd73e938cff
김민서 <sky123039@gamil.com>
2024-05-13 23:17:44 +0900

    회원가입, 로그인, 로그아웃 기능 구현(cookie 사용)

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+const cookieParser = require("cookie-parser")
 
 app.use(express.json()); // JSON 형태의 요청 본문을 파싱
 app.use(express.urlencoded({ extended: true })); // URL-encoded 형태의 요청 본문을 파싱
+app.use(cookieParser())
 
 const { MongoClient } = require('mongodb')
 
@@ -13,7 +15,7 @@ app.use(express.static("./public"))
 // db연결 & 서버실행
 let db
 const url = 'mongodb+srv://voteadmin:vote4519@cluster0.tt2bt5b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-new MongoClient(url).connect().then((client)=>{
+new MongoClient(url).connect().then((client)=>{ 
     console.log('DB연결성공')
     db = client.db('web')
     app.listen(8080, ()=>{
@@ -26,7 +28,8 @@ new MongoClient(url).connect().then((client)=>{
 
 // 라우트 정의
 app.get('/', (req, res)=>{
-    res.render('index')
+    const userNickname = req.cookies.userNickname || null
+    res.render('index', { userNickname: userNickname })
 })
 
 app.get('/search', (req, res)=>{
@@ -42,15 +45,15 @@ app.get('/login', (req, res)=>{
 })
 
 app.post('/login', async(req, res) => {
-    let id = await db.collection('user').findOne({ userId: req.body.id })
-    console.log(id)
-    if (!id) {
-        return cb(null, false, { message: '아이디 또는 비밀번호가 틀렸습니다.' })
+    let userinfo = await db.collection('user').findOne({ userId: req.body.id })
+    if (!userinfo) {
+        res.send('<script>alert("아이디 또는 비밀번호가 틀렸습니다."); location.href="/login";</script>')
     }else{
-        if(result.pw == req.body.password){
-            res.redirect('/')
+        if(userinfo.pw == req.body.password){
+            res.cookie('userNickname', userinfo.nickname, { maxAge: 900000, path: '/' })
+            res.send('<script>alert("로그인 성공!"); location.href="/";</script>')
         }else{
-            alert("아이디 또는 비밀번호가 틀렸습니다.")
+            res.send('<script>alert("아이디 또는 비밀번호가 틀렸습니다."); location.href="/login";</script>')
         }
     }
 })
@@ -59,11 +62,20 @@ app.get('/signup', (req, res)=>{
     res.render('signup')
 })
 
-app.post('/signup', (req, res)=>{
-    db.collection('user').insertOne({
-        userId : req.body.id,
-        pw : req.body.pw,
-        nickname: req.body.nickname
-    })
-    alert("회원가입 완료!")
+app.post('/signup', async(req, res)=>{
+    let userinfo = await db.collection('user').findOne({ userId: req.body.id })
+    if (userinfo) {
+        res.send('<script>alert("이미 있는 아이디입니다."); location.href="/signup";</script>')
+    }else{
+        db.collection('user').insertOne({
+            userId : req.body.id,
+            pw : req.body.pw,
+            nickname: req.body.nickname
+        })
+        res.send('<script>alert("회원가입 완료!"); location.href="/";</script>')
+    }
+})
+
+app.get('/logout', (req, res) => {
+    res.clearCookie('userNickname', { path: '/' }).redirect('/')
 })
