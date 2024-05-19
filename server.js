7a987bf10b058ab8ec70102653f1ddc32227e437
김민서 <sky123039@gamil.com>
2024-05-19 13:01:44 +0900

    1차 완성

--- server.js
+++ server.js
@@ -6,16 +6,12 @@ app.use(express.json()); // JSON 형태의 요청 본문을 파싱
 app.use(express.urlencoded({ extended: true })); // URL-encoded 형태의 요청 본문을 파싱
 app.use(cookieParser())
 
-const { MongoClient } = require('mongodb')
+const { MongoClient, ObjectId } = require('mongodb')
 
 const cron = require('node-cron');
 
 app.set('view engine', 'ejs')
 app.use(express.static("./public"))
-
-// const bodyParser = require('body-parser');
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
 
 // db연결 & 서버실행
 let db, client
@@ -42,60 +38,60 @@ app.get('/', async(req, res)=>{
 app.post('/vote', async (req, res) => {
     const userNickname = req.cookies.userNickname || null
 
+
     if (!userNickname) { // 로그인이 되어있는지 여부
         res.send('<script>alert("로그인을 해야 사용할 수 있는 기능입니다!"); location.href="/login";</script>')
     } else {
-
-        const collection = db.collection('post');
-        let voteed = await collection.findOne({ voteUser: userNickname })
         const voteId = req.body.voteId;
         const itemIndex = req.body.itemIndex;
-        const documentIndex = req.body.documentIndex;
-        
-        if (voteed) { // 이미 투표를 했는지 여부
-            let index = Number(voteed.voteUser.indexOf(userNickname));
-            console.log(Number(itemIndex), index)
-            if (Number(itemIndex) != index) { // 이미 투표하고있지만 다른 항목을 투표하는경우 -> 투표 옮기기
 
-                await collection.update(
-                    { "voteContent": index },
-                    { 'voteContent.$': itemIndex }
-                );
+        const collection = db.collection('post')
 
-            } else { // 이미 투표했지만 같은 항목을 투표하는 경우 -> 투표 취소
+        let voted = await collection.findOne({ _id: new ObjectId(voteId) });
 
-                await collection.updateOne(
-                    { _id: new ObjectId(voteId) },
-                    {
-                        $inc: { total: -1 },
-                        // $pull: { voteUser: userNickname, voteContent: index }
-                    }
-                );
+        let currentIndex = voted.voteContent[voted.voteUser.indexOf(userNickname)];
 
-            }
-        } else { // 투표를 한적이 없다
+        if (currentIndex !== undefined && currentIndex.toString() === itemIndex) {
+            return
 
-            const { ObjectId } = require('mongodb');
+        } else if (currentIndex !== undefined) {
+
             await collection.updateOne(
                 { _id: new ObjectId(voteId) },
-                {
-                    $inc: { [`votes.${itemIndex}`]: 1, "total": 1 },
-                    $push: { "voteUser": userNickname, "voteContent": itemIndex }
+                { 
+                    $set: { [`voteContent.${voted.voteUser.indexOf(userNickname)}`]: itemIndex }
                 }
+            );
+        } else {
+
+            await collection.updateOne(
+                { _id: new ObjectId(voteId) },
+                { $push: { "voteUser": userNickname, "voteContent": itemIndex } }
             );
         }
 
         const update = await collection.findOne({ _id: new ObjectId(voteId) });
 
-        res.send({ update: update, documentIndex: documentIndex, userNickname: userNickname });
+        const votes = [0, 0, 0, 0, 0]
+
+        update.voteContent.forEach(e => {
+            votes[Number(e)] ++;
+        });
+
+        const total = votes[0] + votes[1] + votes[2] + votes[3] + votes[4]
+
+        res.send({ update: update, documentIndex: req.body.documentIndex, userNickname: userNickname, votes: votes, total: total, itemIndex: itemIndex});
 
     }
         }
 );
 
-app.get('/closed', (req, res)=>{
+app.get('/finished', async(req, res)=>{
     const userNickname = req.cookies.userNickname || null
-    res.render('search', { userNickname: userNickname })
+
+    let voteInfo = await db.collection('finished').find().sort({date: -1}).toArray()
+
+    res.render('finished', { userNickname: userNickname, voteInfo: voteInfo })
 })
 
 app.get('/make', (req, res)=>{
@@ -105,6 +101,38 @@ app.get('/make', (req, res)=>{
     } else {
         res.render('make', { userNickname: userNickname })
     }
+})
+
+app.post('/upload', (req, res) => {
+    const userNickname = req.cookies.userNickname || null
+
+    if (!userNickname) {
+        return res.send('<script>alert("로그인이 필요합니다."); location.href="/login";</script>')
+    }
+
+    
+    const VOTEITEM = []; // 투표 항목마다 내용
+    const VOTES = []; // 투표 항목마다 몇표를 받았는지
+    
+    for (let i = 1; i <= 5; i+=1) {
+        const voteItemName = `vote${i}`;
+        if (req.body[voteItemName]) {
+            VOTEITEM.push(req.body[voteItemName]);
+            VOTES.push(0);
+        }
+    }
+
+    db.collection('post').insertOne({
+        nickname : userNickname, // 투표를 올린 유저의 닉네임
+        content : req.body.content, // 투표의 제목같은 내용적는 곳
+        voteItem: VOTEITEM, // 투표의 항목들
+        voteUser: [], // 투표를 한 유저
+        voteContent: [], // 투표를 한 유저가 어떤 항목에 투표했는지
+        date: Date.now()
+    })
+    res.send('<script>alert("업로드 완료!"); location.href="/";</script>')
+
+
 })
 
 app.get('/login', (req, res)=>{
@@ -152,61 +180,29 @@ app.get('/logout', (req, res) => {
     res.clearCookie('userNickname', { path: '/' }).redirect('/')
 })
 
-app.post('/upload', (req, res) => {
-    const userNickname = req.cookies.userNickname || null
-
-    if (!userNickname) {
-        return res.send('<script>alert("로그인이 필요합니다."); location.href="/login";</script>')
-    }
-
-    
-    const VOTEITEM = []; // 투표 항목마다 내용
-    const VOTES = []; // 투표 항목마다 몇표를 받았는지
-    
-    for (let i = 1; i <= 5; i+=1) {
-        const voteItemName = `vote${i}`;
-        if (req.body[voteItemName]) {
-            VOTEITEM.push(req.body[voteItemName]);
-            VOTES.push(0);
-        }
-    }
-
-    db.collection('post').insertOne({
-        nickname : userNickname, // 투표를 올린 유저의 닉네임
-        content : req.body.content, // 투표의 제목같은 내용적는 곳
-        voteItem: VOTEITEM, // 투표의 항목들s
-        votes: VOTES, // 항목마다 받은 투표 순
-        total: 0, // 투표를 한 유저의 수
-        voteUser: [], // 투표를 한 유저
-        voteContent: [], // 투표를 한 유저가 어떤 항목에 투표했는지
-        date: Date.now()
-    })
-    res.send('<script>alert("업로드 완료!"); location.href="/";</script>')
 
 
-})
 
+cron.schedule('0 * * * * *', async () => {
 
-// post에 있는 3일이 지난 데이터를 closed 컬렉션으로 이동
-async function moveData() {
+    const threeDaysAgo = new Date(Date.now() - (72 * 60 * 60 * 1000));
+
     try {
-        const source = db.collection("post");
-        const target = db.collection("closed");
-
-        const threeDaysAgo = new Date(new Date().getTime() - (3 * 24 * 60 * 60 * 1000)); // 지금으로부터 3일전 날짜 계산
-        const query = { "date": { $lt: threeDaysAgo } };
-        const documentsToMove = await source.find(query).toArray(); // date 값이 3일보다 이전인 문서를 찾는다.
+        // 'post' 컬렉션에서 3일이 지난 문서들을 찾음
+        const documentsToMove = await db.collection('post').find({
+            date: { $lt: threeDaysAgo.getTime() }
+        }).toArray();
 
         if (documentsToMove.length > 0) {
-            await target.insertMany(documentsToMove);
-            await source.deleteMany(query);
+            // 각 문서를 'finished' 컬렉션에 추가
+            await db.collection('finished').insertMany(documentsToMove);
+
+            // 추가된 문서들을 'post' 컬렉션에서 삭제
+            await Promise.all(documentsToMove.map(document => {
+                return db.collection('post').deleteOne({ _id: document._id });
+            }));
         }
-    } finally {
+    } catch (error) {
         return
     }
-}
-
-
-cron.schedule('0 * * * * *', () => {
-    moveData();
 });
